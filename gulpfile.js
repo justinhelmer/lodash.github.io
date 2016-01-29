eef920d906597939eb7bb81ddc0fa149e5e92e5b
justinhelmer <jhelmer25@gmail.com>
2016-01-28 18:04:00 -0800

    set up JSCS for code style linting
    - conform to google javascript styleguide
    - automate during build routine
    - cancel build on linting failure
    - set up WebStorm integration

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,27 @@
 
   var gulp = require('gulp');
   var fork = require('./lib/fork');
+  var jscs = require('gulp-jscs');
+  var notify = require('gulp-notify');
   var buildPath = '_site';
 
-  gulp.task('build', build);
-  gulp.task('copySCSS', copySCSS);
+  gulp.task('build', ['lintStyle', 'copySCSS']);
+  gulp.task('lintStyle', lintStyle);
+  gulp.task('copySCSS', ['lintStyle'], copySCSS);
 
-  function build() {
-    return copySCSS();
+  function copySCSS() {
+    var files = ['node_modules/normalize.css/normalize.css'];
+    gulp.src(files).pipe(gulp.dest(buildPath + '/css'));
   }
 
-  /**
-   * Seemingly strange, but Jekyll is handling the CSS preprocessing and thus, needs additional CSS in the source.
-   */
-  function copySCSS() {
-    var files = [
-      'node_modules/normalize.css/normalize.css'
-    ];
+  function lintStyle() {
+    var files = ['bin/*.js', 'lib/*.js', 'gulpfile.js'];
 
-    return gulp.src(files).pipe(gulp.dest(buildPath + '/css'));
+    return gulp
+        .src(files)
+        .pipe(jscs())
+        .pipe(require('gulp-jscs-stylish')())
+        .pipe(jscs.reporter('fail'));
+
   }
 })();
