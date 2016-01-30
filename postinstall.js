7cb1cb13656f38f78e6db45c1f3ff2c22cad24e8
justinhelmer <jhelmer25@gmail.com>
2016-01-29 18:35:18 -0800

    remove postinstall
    - redunant
    - confusing
    - innacurate

--- postinstall.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-console.log('\
----------------------------------------------------------------------------\n\n\
-Assuming there were no errors above, all dependencies have been installed,\n\
-and all test suites passed. If there are errors, correct them. Else, you\n\
-can now build and launch the app using `npm start`.\n\n\
-To add `lodocs` to your $PATH, run `npm link`.');
