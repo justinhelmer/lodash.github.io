7984e427a31dc5f4708ab5fa27fec100ae6b2ac0
justinhelmer <jhelmer25@gmail.com>
2016-02-09 14:58:45 -0800

    configure eslintrc.js for project-specific guidelines:
    - extends all of the google styleguide settings
    - enables the mocha environment for registering known mocha globals
    - enables the mocha plugin for additional mocha-specific linting rules
    - configures all applicable mocha linting rules
    - configures line length max to be 150 chars instead of the default 80

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,18 @@
-module.exports = {
-  extends: 'google'
-};
+(function() {
+  'use strict';
+
+  module.exports = {
+    extends: 'google',
+    env: {
+      mocha: true
+    },
+    plugins: ['mocha'],
+    rules: {
+      'max-len': [2, 150, 2],
+      'mocha/handle-done-callback': 2,
+      'mocha/no-exclusive-tests': 2,
+      'mocha/no-global-tests': 2
+    }
+  };
+})();
+
